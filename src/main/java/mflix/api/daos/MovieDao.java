881ec3117c6aa40157baa5a04389a8d5bcde1557
lytves <22356770+lytves@users.noreply.github.com>
2019-02-20 17:37:10 +0100

    chapter 2: Facets Search Ticket Resolved

--- src/main/java/mflix/api/daos/MovieDao.java
+++ src/main/java/mflix/api/daos/MovieDao.java
@@ -276,21 +276,28 @@ public class MovieDao extends AbstractMFlixDao {
      * movies: {$addFields: ...}, }} ])
      */
     public List<Document> getMoviesCastFaceted(int limit, int skip, String... cast) {
+
         List<Document> movies = new ArrayList<>();
+
         String sortKey = "tomatoes.viewer.numReviews";
         Bson skipStage = Aggregates.skip(skip);
         Bson matchStage = Aggregates.match(Filters.in("cast", cast));
         Bson sortStage = Aggregates.sort(Sorts.descending(sortKey));
         Bson limitStage = Aggregates.limit(limit);
         Bson facetStage = buildFacetStage();
+
         // Using a LinkedList to ensure insertion order
         List<Bson> pipeline = new LinkedList<>();
 
-        // TODO > Ticket: Faceted Search - build the aggregation pipeline by adding all stages in the
+        // RESOLVED TODO > Ticket: Faceted Search - build the aggregation pipeline by adding all stages in the
         // correct order
         // Your job is to order the stages correctly in the pipeline.
         // Starting with the `matchStage` add the remaining stages.
         pipeline.add(matchStage);
+        pipeline.add(sortStage);
+        pipeline.add(skipStage);
+        pipeline.add(limitStage);
+        pipeline.add(facetStage);
 
         moviesCollection.aggregate(pipeline).iterator().forEachRemaining(movies::add);
         return movies;
